188c8ec0e31781581cd7b73ceb7c2dbda3591f5c
Christopher Rumble <christopher.rumble@MacBook-Pro.hsd1.ca.comcast.net>
2021-01-12 21:37:11 -0800

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,23 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// int int2=5;
+	// false true null
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        //variables(kilometers), values(1.609344), and operators(=,*) = expression
+        // keywords as-in Java's 53 Reserved Words
+        int highScore = 50;
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+// In the following code, write down which parts of the code are expressions.
+        int score = 100;
+        if (score > 99) {
+            System.out.println("You got the high score!");
+            score = 0;
+        }
+    }
+}
